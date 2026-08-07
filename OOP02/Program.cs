f5341be4636e80e151f5e49cf7b5ee8fab21e175
JanaShebl <janamohamed195200@gmail.com>
2026-08-07 13:11:12 +0300

    Q1

--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+
+            // a) What is the difference between a class and a struct? 
+
+            // class => reference type , allows inheritance , can be null , stores in haep ,copy the reference
+            // struct => value type , doesn't allow inheritance , can't be null,stores in stack , copy the value
+
+            // b) Why are classes more suitable than structs for large applications? 
+
+            // classes allow inheritance and it's more suitable for complex projects
+
+            #endregion
+
+
         }
     }
 }
